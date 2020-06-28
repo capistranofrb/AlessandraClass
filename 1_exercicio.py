894fd69d442224d9b4c2e8952cf6505c34dc7c43
capistrano-frb <capistrano.frb@outlook.com>
2020-06-28 17:52:34 -0300

    Create 1_exercicio.py
    Commit 1

---
+++ 1_exercicio.py
@@ -0,0 +1,53 @@
+"""
+#Exercício 1
+#1
+print("Hello World!"
+#Resp.: SyntaxError: unexpected EOF while parsing
+
+#2
+print(Hello World)
+#Resp.: SyntaxError: invalid syntax
+
+#3
+x = +2
+y = 2++2
+print(x, y)
+
+#4
+x = 02
+print(x)
+# Resp.: SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+
+#5
+x = 2 6
+print(x)
+#Resp.: SyntaxError: invalid syntax
+
+#Exercício 2
+#1
+m = 60
+s = 42 * m + 42
+print(s)
+#Resp.: 2562
+
+#2
+mi = 1.61
+km = 10
+mk = km / mi
+print(mk)
+#Resp.: 6.211180124223602
+
+#3
+mi = 6.211180124223602
+tp = 2562
+passo = mi / tp
+passo = mi / (tp*(1/60))
+passo = mi / (tp*(1/3600))
+print(passo)
+#Resp.: Segundos -> 0.0024243482139826703
+#Minutos -> 0.14546089283896022
+#Velocidade Média -> 8.727653570337614 mi/h
+"""
+
+
+
